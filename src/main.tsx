89073a00d9f1eee7fb9e8f2f1c7506501c3d656d
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-27 23:13:04 +0500

    Add theme & set up main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
   </StrictMode>,
 );
